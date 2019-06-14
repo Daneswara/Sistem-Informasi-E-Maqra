d7af387893d26edc2a93e92ab56bf502d7869b5a
Danes <daneswarajauhari@gmail.com>
2019-06-14 20:38:41 +0700

    update - tidak bisa akses halaman utama kalau belum login

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,6 +18,17 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+
+        $email = $this->session->userdata('email');
+        if (!isset($email)) {
+            redirect(base_url('index.php/User'));
+        }
+    }
+
 	public function index()
 	{
 		$this->load->view('template');
