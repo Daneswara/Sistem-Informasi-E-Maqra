ac0554ca3770df6066ec540cc43ad8ed5f14da31
Danes <daneswarajauhari@gmail.com>
2019-06-27 20:07:54 +0700

    perbaiki tampilan

--- application/views/_template/head.php
+++ application/views/_template/head.php
@@ -135,34 +135,19 @@
                         <li class="dropdown user user-menu">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                 <img src="<?php echo base_url('asset/adminlte/dist/img/user2-160x160.jpg') ?>" class="user-image" alt="User Image"/>
-                                <span class="hidden-xs">Alexander Pierce</span>
+                                <span class="hidden-xs"><?php echo $this->session->userdata('email'); ?></span>
                             </a>
                             <ul class="dropdown-menu">
                                 <!-- User image -->
                                 <li class="user-header">
                                     <img src="<?php echo base_url('asset/adminlte/dist/img/user2-160x160.jpg') ?>" class="img-circle" alt="User Image" />
                                     <p>
-                                        Alexander Pierce - Web Developer
-                                        <small>Member since Nov. 2012</small>
+                                        <?php echo $this->session->userdata('email'); ?>
+                                        <small><?php echo $this->session->userdata('access'); ?></small>
                                     </p>
-                                </li>
-                                <!-- Menu Body -->
-                                <li class="user-body">
-                                    <div class="col-xs-4 text-center">
-                                        <a href="#">Followers</a>
-                                    </div>
-                                    <div class="col-xs-4 text-center">
-                                        <a href="#">Sales</a>
-                                    </div>
-                                    <div class="col-xs-4 text-center">
-                                        <a href="#">Friends</a>
-                                    </div>
                                 </li>
                                 <!-- Menu Footer-->
                                 <li class="user-footer">
-                                    <div class="pull-left">
-                                        <a href="<?php echo base_url('index.php/User')?>" class="btn btn-default btn-flat">Profile</a>
-                                    </div>
                                     <div class="col-xs-2 text-center">
                                         <a href="<?php echo base_url('index.php/User/Ganti')?>" class="btn btn-default btn-flat">Ganti Password</a>
                                     </div>
